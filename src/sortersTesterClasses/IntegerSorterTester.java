e02b128ab4aeb26b99261777535d4e13ff501e6e
Kariel Argenis Díaz Maisonet <kariel.diaz@upr.edu>
2018-03-19 13:09:58 -0400

    Completed Exercise 1

--- src/sortersTesterClasses/IntegerSorterTester.java
+++ src/sortersTesterClasses/IntegerSorterTester.java
@@ -12,7 +12,8 @@ import interfaces.Sorter;
 public class IntegerSorterTester {
 	//private static Sorter<Integer> sorter; 
 	private static Random rnd; 
-	private static ArrayList<Sorter<Integer>> sortersList = new ArrayList<>(); 
+	private static ArrayList<Sorter<Integer>> sortersList = new ArrayList<>();
+	private static ArrayList<Sorter<Entero>> sortersList2 = new ArrayList<>();
 	
 	public static void main(String[] args) { 
 		sortersList.add(new BubbleSortSorter<Integer>()); 
@@ -22,6 +23,13 @@ public class IntegerSorterTester {
 		test("Sorting Using Default Comparator<Integer>", null); 
 		test("Sorting Using IntegerComparator1", new IntegerComparator1()); 
 		test("Sorting Using IntegerComparator2", new IntegerComparator2()); 
+		
+		///////Testing array of type Entero[]
+		sortersList2.add(new BubbleSortSorter<Entero>()); 
+		sortersList2.add(new SelectionSortSorter<Entero>()); 
+		sortersList2.add(new InsertionSortSorter<Entero>()); 
+		
+		test2("Sorting Using Default Comparator<Integer>", null); 
 	}
 	
 	private static void test(String msg, Comparator<Integer> cmp) { 
@@ -59,5 +67,42 @@ public class IntegerSorterTester {
 			a[j] = rnd.nextInt(100); 
 		return a;
 	}
+	
+	//////////////////////////////////////////////////////////////////////////////
+	//Tester for array of type Entero[]
+	private static void test2(String msg, Comparator<Entero> cmp) { 
+		rnd = new Random(101); 
 
+		System.out.println("\n\n*******************************************************");
+		System.out.println("*** " + msg + "  ***");
+		System.out.println("*******************************************************");
+		
+		Entero[] original, arr; 
+		// generate random arrays is size i and test...
+		for (int i=1; i<=20; i += 5) { 
+			original = randomValores(i);
+			showEnteroArray("\n ---Original array of size " + i + " to sort:", original); 
+			
+			for (int s=0; s<sortersList2.size(); s++) {
+				Sorter<Entero> sorter = sortersList2.get(s); 
+			    arr = original.clone(); 
+			    sorter.sort(arr, cmp);
+			    showEnteroArray(sorter.getName() + ": ", arr); 
+			}
+		}
+	}
+
+	private static void showEnteroArray(String msg, Entero[] a) {
+		System.out.print(msg); 
+		for (int i=0; i<a.length; i++) 
+			System.out.print("\t" + a[i].getValue()); 
+		System.out.println();
+	}
+
+	private static Entero[] randomValores(int i) {
+		Entero[] a = new Entero[i]; 
+		for (int j=0; j<i; j++) 
+			a[j] = new Entero(rnd.nextInt(100)); 
+		return a;
+	}
 }
